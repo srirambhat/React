f405a2586ca0852b66db56d4f853d0ba6b5507a0
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-22 14:10:19 -0800

    Update Post Chapter3

--- hellotutorial/src/App.js
+++ hellotutorial/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './Components/Employee';
 
 function App() {
+  console.log('We are about to list the employee');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('inside the return')}
+      { showEmployees ? (
+      <>
+      
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      )  : (
+        <p> You cannot see the employee</p>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
