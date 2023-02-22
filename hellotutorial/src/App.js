52d301b5263b955de3d0353f20f885669088a5f1
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-22 15:28:55 -0800

    After Image Addition

--- hellotutorial/src/App.js
+++ hellotutorial/src/App.js
@@ -7,7 +7,7 @@ function App() {
   console.log('We are about to list the employee');
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {console.log('inside the return')}
       { showEmployees ? (
         <>
@@ -18,10 +18,15 @@ function App() {
             } 
           }
         />
-        <Employee name = "Sriram Bhat" role ={role}/>
-        <Employee name = "Maithri Bhat" role="Mom"/>
-        <Employee name = "Tushar Bhat" role="Son"/>
-        <Employee name = "Pranati Bhat" />
+        <div className = "flex flex-wrap justify-center">
+          <Employee name = "Sriram Bhat" role ={role} img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Maithri Bhat" role="Mom" img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Tushar Bhat" role="Son" img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Pranati Bhat" img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Sriram Bhat" role ={role} img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Maithri Bhat" role="Mom" img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+          <Employee name = "Tushar Bhat" role="Son" img="https://images.pexels.com/photos/3291250/pexels-photo-3291250.jpeg"/>
+        </div>
       </>
     )  : (
         <p> You cannot see the employee</p>
