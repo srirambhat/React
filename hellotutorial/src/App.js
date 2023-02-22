d20612f696ba05583ef1050701a9efce29b3ed6f
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-22 14:40:31 -0800

    Using States

--- hellotutorial/src/App.js
+++ hellotutorial/src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import Employee from './Components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRol] = useState('idk');
   console.log('We are about to list the employee');
   const showEmployees = true;
   return (
     <div className="App">
       {console.log('inside the return')}
       { showEmployees ? (
-      <>
-      
-          <Employee name = "Sriram Bhat" role ="Dad"/>
-          <Employee name = "Maithri Bhat" role="Mom"/>
-          <Employee name = "Tushar Bhat" role="Son"/>
-          <Employee name = "Pranati Bhat" />
-        </>
-      )  : (
+        <>
+          <input type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRol(e.target.value);
+            } 
+          }
+        />
+        <Employee name = "Sriram Bhat" role ={role}/>
+        <Employee name = "Maithri Bhat" role="Mom"/>
+        <Employee name = "Tushar Bhat" role="Son"/>
+        <Employee name = "Pranati Bhat" />
+      </>
+    )  : (
         <p> You cannot see the employee</p>
       )}
     </div>
