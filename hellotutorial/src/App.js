f3f6ea9b78585531cd02a93378821145f2321b87
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-23 13:06:17 -0800

    Add brorzerroutes and routes

--- hellotutorial/src/App.js
+++ hellotutorial/src/App.js
@@ -3,13 +3,21 @@ import Employees from './pages/Employees';
 import Header from './Components/Header';
 import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Customers from './pages/Customers';
 
 
 function App() {
   
   return (
     <Header>
-      <Employees />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/employees' element={<Employees/>} />
+        <Route path='/customers' element={<Customers/>} />
+      </Routes>
+      </BrowserRouter>
+      
     </Header>
       
     
