03ba1e6e431c29c47d37e2c312ff0575357af051
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-23 15:55:40 -0800

    Multiple useEffect Ways to use

--- hellotutorial/src/Components/Dictionary.js
+++ hellotutorial/src/Components/Dictionary.js
@@ -2,13 +2,25 @@ import { useState, useEffect } from "react";
 
 export default function Dictionary() 
 {
-    const [word, setWord] = useState('Help');
+    const [word, setWord] = useState('');
+    const [word2, setWord2] = useState('');
 
     useEffect(
         () => {
-            console.log('State Updated' + ' ' + word);
+            console.log('State Updated ' + 'word: ' + word);
         }
-    );
+    , [word]);
+
+    useEffect(
+        () => {
+            console.log('State Updated ' + ' word2: ' +word2);
+        }
+    , [word2]);
+
+    // 3 ways of using useEffect
+    // no dependancy array =>  for any state change
+    // empty dependacny array --> execute once
+    // one or more entry's in the array --> execute for those in the array
 
     return (
         <>
@@ -17,6 +29,12 @@ export default function Dictionary()
                 }}
             />
             <h1> Lets get the definition of the {word} </h1>
+
+            <input type="test" onChange={(e) => {
+                setWord2(e.target.value);
+                }}
+            />
+            <h1> Lets get the definition of the {word2} </h1>
         </>
     )
 }
