4504b59977efa2732f2527b040360410a754fbe6
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-27 11:54:13 -0800

    Display the drop down for displaying values

--- cryptocurrencies/src/App.tsx
+++ cryptocurrencies/src/App.tsx
@@ -6,7 +6,9 @@ import CryptoSummary from './Components/CryptoSummary';
 import { Crypto } from './Types';
 
 function App() {
-    const [cryptos, setCryptos] = useState<Crypto[] | null>();
+    const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+    const [selected, setSelected] = useState<Crypto | null>();
+
     useEffect(() => {
         const url =
             'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false';
@@ -18,13 +20,30 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            {cryptos
-                ? cryptos.map((crypto) => {
-                      return <CryptoSummary crypto={crypto}></CryptoSummary>;
-                  })
-                : null}
-        </div>
+        <>
+            <div className="App">
+                <select
+                    onChange={(e) => {
+                        const c = cryptos?.find((x) => x.id === e.target.value);
+                        console.log(c);
+                        setSelected(c);
+                    }}
+                    defaultValue="default"
+                >
+                    <option value="default"> Choose an Option</option>
+                    {cryptos
+                        ? cryptos.map((crypto) => {
+                              return (
+                                  <option key={crypto.id} value={crypto.id}>
+                                      {crypto.name}
+                                  </option>
+                              );
+                          })
+                        : null}
+                </select>
+            </div>
+            {selected ? <CryptoSummary crypto={selected} /> : null}
+        </>
     );
 }
 
