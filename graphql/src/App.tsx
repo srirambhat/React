20edf8d376e1e32ce6f6bdd8e4b1a8ed99116978
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-27 21:42:49 -0800

    Add graphQL client

---
+++ graphql/src/App.tsx
@@ -0,0 +1,50 @@
+import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
+
+export type Launch = {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    };
+};
+
+const GET_DATA = gql`
+    {
+        launchesPast(limit: 10) {
+            mission_name
+            launch_date_local
+            launch_site {
+                site_name_long
+            }
+        }
+    }
+`;
+
+function App() {
+    const { loading, error, data } = useQuery(GET_DATA);
+
+    useEffect(() => {
+        console.log(loading, error, data);
+    });
+
+    return (
+        <div className="App">
+            {data
+                ? data.launchesPast.map((launch: Launch) => {
+                      return (
+                          <p>
+                              {' '}
+                              {launch.mission_name +
+                                  ' ' +
+                                  launch.launch_date_local}
+                          </p>
+                      );
+                  })
+                : null}
+        </div>
+    );
+}
+
+export default App;
---
+++ graphql/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    gql,
+} from '@apollo/client';
+
+let client = new ApolloClient({
+    //uri: 'https://main--spacex-l4uc6p.apollographos.net/graphql ',
+    // uri: 'https://api.spacexdata.com/v5/launches/latest',
+    uri: 'https://api.spacex.land/graphql',
+    cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
