0f513fa8b4cc78bc3f5ee1829d1af8922022812d
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-28 17:13:31 -0800

    Initial Commit

---
+++ customernjs/pages/customers/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import styles from '../../styles/Home.module.css';
+
+const Customers: NextPage = () => {
+    return <div>Customers</div>;
+};
+
+export default Customers;
