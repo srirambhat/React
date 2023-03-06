f3699dc267296f4b19b9fd7615b82936ec918188
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-03-06 15:01:20 -0800

    Add getStaticProps and GetStaticPaths

--- customernjs/pages/customers/[id].tsx
+++ customernjs/pages/customers/[id].tsx
@@ -2,17 +2,20 @@ import type { GetStaticProps, GetStaticPaths, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { ScriptProps } from 'next/script';
 import { Customer } from '../customers';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { ParsedUrlQuery } from 'querystring';
 
 type Props = {
-    customer: Customer;
+    customer?: Customer;
 };
 
 interface Params extends ParsedUrlQuery {
     id: string;
 }
 
+/*
+ * Which of the ID's must you return the data for. If not, then it will return a 404 error on client
+ */
 export const getStaticPaths: GetStaticPaths = async () => {
     const result = await axios.get('http://127.0.0.1:8000/api/customers/');
     const paths = result.data.customers.map((customer: Customer) => {
@@ -22,35 +25,49 @@ export const getStaticPaths: GetStaticPaths = async () => {
     });
 
     return {
-        //        paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
         paths: paths,
-        fallback: false,
+        fallback: true,
     };
 };
+
 export const getStaticProps: GetStaticProps<Props, Params> = async (
     context
 ) => {
     const params = context.params!;
 
-    // request for the data --> dB, API
-    const result = await axios.get<{ customer: Customer }>(
-        `http://127.0.0.1:8000/api/customers/${params.id}`
-    );
-    console.log('Restult1:', result);
+    try {
+        const result = await axios.get<{ customer: Customer }>(
+            `http://127.0.0.1:8000/api/customers/${params.id}`
+        );
 
-    return {
-        props: {
-            customer: result.data.customer.name,
-        },
-    };
+        console.log('Returned from Server: ', result);
+        return {
+            props: {
+                customer: result.data.customer,
+            },
+        };
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            if (error.response?.status === 404) {
+                return {
+                    notFound: true,
+                };
+            }
+        }
+        return {
+            props: {},
+        };
+    }
 };
 
 const Customer: NextPage<Props> = (props) => {
     const router = useRouter();
-    const { id } = router.query;
 
-    console.log(props);
-    return <div>Customer {props.customer} </div>;
+    if (router.isFallback) {
+        return <p> Loading...</p>;
+    }
+
+    return <div>Customer {props.customer ? props.customer.name : null} </div>;
 };
 
 export default Customer;
