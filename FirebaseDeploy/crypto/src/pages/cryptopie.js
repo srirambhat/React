6029579c8da814884c49393fb4b5c046fb7e10b6
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-03-22 21:24:46 -0700

    Basic working Cryptopie

--- FirebaseDeploy/crypto/src/pages/cryptopie.js
+++ FirebaseDeploy/crypto/src/pages/cryptopie.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import moment from 'moment';
 import {
     ResponsiveContainer,
-    LineChart,
-    Line,
+    PieChart,
+    Pie,
     BarChart,
     Bar,
     CartesianGrid,
@@ -12,17 +12,157 @@ import {
     YAxis,
     Tooltip,
     Legend,
+    LineChart,
 } from 'recharts';
+import CryptoSummary from '../Components/Cryptosummary';
 
 const cnstdata = [
-    { label: 'January', sales: 21, leads: 41 },
-    { label: 'February', sales: 35, leads: 79 },
-    { label: 'March', sales: 75, leads: 57 },
-    { label: 'April', sales: 51, leads: 47 },
-    { label: 'May', sales: 41, leads: 63 },
-    { label: 'June', sales: 47, leads: 71 },
+    { dateortime: 'January', price: 21 },
+    { dateortime: 'February', price: 35 },
+    { dateortime: 'March', price: 75 },
+    { dateortime: 'April', price: 51 },
+    { dateortime: 'May', price: 41 },
+    { dateortime: 'June', price: 47 },
 ];
 
 export default function Cryptopie() {
-    return <h1> Cryptopie</h1>;
+    const [cryptos, setCryptos] = useState();
+    const [range, setRange] = useState(30);
+    const [selected, setSelected] = useState(false);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const url =
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+        axios.get(url).then((response) => {
+            setCryptos(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!selected) return;
+        axios
+            .get(
+                `https://api.coingecko.com/api/v3/coins/${
+                    selected?.id
+                }/market_chart?vs_currency=usd&days=${range}&${
+                    range === 1 ? 'interval=hourly' : `interval=daily`
+                }`
+            )
+            .then((response) => {
+                console.log('Response: ', response.data);
+
+                setData(
+                    response.data.prices.map((price) => {
+                        var dt = moment
+                            .unix(price[0] / 1000)
+                            .format(range === 1 ? 'HH:MM' : 'MM-DD');
+                        var pr = parseInt(price[1].toFixed(2));
+                        var i = {
+                            dateortime: dt,
+                            price: pr,
+                        };
+                        return i;
+                    })
+                );
+            });
+    }, [selected, range]);
+
+    return (
+        <>
+            <div className="mx-3 ">
+                <select
+                    className="px-2 py-1 mr-3 mt-4 rounded-md bg-gray-800 text-white "
+                    onChange={(e) => {
+                        const c = cryptos?.find((x) => x.id === e.target.value);
+                        console.log(c);
+                        setSelected(c);
+                    }}
+                    defaultValue="default"
+                >
+                    <option value="default">Choose an option</option>
+                    {cryptos
+                        ? cryptos.map((crypto) => {
+                              return (
+                                  <option key={crypto.id} value={crypto.id}>
+                                      {crypto.name}
+                                  </option>
+                              );
+                          })
+                        : null}
+                </select>
+                <select
+                    className="px-1 py-1 mr-3 bg-gray-800 rounded-md text-white"
+                    onChange={(e) => {
+                        setRange(parseInt(e.target.value));
+                    }}
+                >
+                    <option value={30}>30 Days</option>
+                    <option value={7}>7 Days</option>
+                    <option value={1}>1 Day</option>
+                </select>
+            </div>
+            {/* {data ? console.log(data) : null}
+            {console.log(cnstdata)} */}
+            <div className="row mx-3">
+                <div className="col-md-12">
+                    <br></br>
+                    <h4>
+                        {selected
+                            ? `${selected?.name} Price Over Last ` +
+                              range +
+                              (range === 1 ? ' Day.' : ' Days.')
+                            : null}
+                    </h4>
+                </div>
+                {selected ? <CryptoSummary crypto={selected} /> : null}
+
+                {selected ? (
+                    // <div className="section col-md-6">
+                    //     {/* <h3 className="section-title">Line Chart</h3> */}
+                    //     <div className="section-content">
+                    //         <ResponsiveContainer width="150%" height={500}>
+                    //             <LineChart
+                    //                 data={data}
+                    //                 margin={{
+                    //                     top: 15,
+                    //                     right: 0,
+                    //                     bottom: 15,
+                    //                     left: 0,
+                    //                 }}
+                    //             >
+                    //                 <Tooltip />
+                    //                 <XAxis dataKey="dateortime" />
+                    //                 <YAxis />
+                    //                 <CartesianGrid
+                    //                     stroke="#ccc"
+                    //                     strokeDasharray="5 5"
+                    //                 />
+                    //                 <Legend />
+                    //                 <Line
+                    //                     type="monotone"
+                    //                     dataKey="price"
+                    //                     stroke="#FB8833"
+                    //                 />
+                    //             </LineChart>
+                    //         </ResponsiveContainer>
+                    //     </div>
+                    // </div>
+                    <PieChart width={800} height={800}>
+                        <Pie
+                            dataKey="price"
+                            isAnimationActive={true}
+                            data={data}
+                            outerRadius={300}
+                            fill="orangered"
+                            label
+                        />
+
+                        {/* Display the tooltips */}
+                        <Tooltip />
+                    </PieChart>
+                ) : null}
+            </div>
+        </>
+    );
 }
